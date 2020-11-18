e7ce61f86d63b21cf5cab06fcd76045fe7d64d87
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 23:04:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alen_kay][:known_for] = "GUI"
+programmer_hash[:alan_kay][:known_for] = "GUI"
 programmer_hash
 end
 
