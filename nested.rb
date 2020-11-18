a4e96b7bec09695b11074f6c60a821b3c94757d3
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 23:02:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[]
+programmer_hash[:yukihiro_matsumoto] = 
+{
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+programmer_hash
 end
 
 def changing_alan
